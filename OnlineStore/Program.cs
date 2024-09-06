b6a836d6a21ebbf4c6fd399e5fb790fa3059d393
manar-hussein <manarnabil911@gmail.com>
2024-09-06 15:21:33 +0300

    Add Database to Depndancy Injection Container

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -16,6 +16,9 @@ namespace OnlineStore
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<APIStoreDbContext>(option =>
+            option.UseSqlServer(builder.Configuration.GetConnectionString("Dev"))
+            );
 
             var app = builder.Build();
             var scope = app.Services.CreateScope();
